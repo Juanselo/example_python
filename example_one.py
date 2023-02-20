fe998c504fe8c7248ba32dc601017df52c6d5898
Juanselo <125880361+Juanselo@users.noreply.github.com>
2023-02-20 07:42:31 -0500

    variables

---
+++ example_one.py
@@ -0,0 +1,50 @@
+"""
+name = "Jesus María Cordova"
+print("Hello", name)
+
+# ejemplo de casting  (convertir una varable de dato a otro)
+variable = 5
+variable_uno = float (variable)
+variable_dos = str(variable)
+
+#sumar_datos = (variable + variable_dos)
+
+#print(variable, variable_uno)
+print(variable, variable_uno, variable_dos)
+print(type(variable_dos))
+print(type (variable_uno))
+print(type(variable))
+#print(sumar_datos)
+
+"""
+
+#tipos de datos
+variable = "Hola" #str (string)
+variable1 = 20  #int (entero)
+variable2 = 20.8 #float (decimal)
+variable4 = 1j  #complex ()
+variable5 = ["cantar","Jugar", "bailar"]
+variable6 = ("cantar","Jugar", "bailar")
+variable7 = False #bool (logico) 
+carro = {"color": 'rojo', "velocidad": 300, "Modelo": 'Ford', }
+
+#print(variable5)
+#print(variable6)
+print(variable7)
+#print(c
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# arro)
+
